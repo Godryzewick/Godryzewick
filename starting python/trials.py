7da06396e1990b05f2d0248f69a86d32fa8100d8
Godryzewick@ <godwintitus05@gmail.com>
2021-08-04 22:59:13 +0530

    addition of numder

---
+++ starting python/trials.py
@@ -0,0 +1,8 @@
+print("welcome to math with kat")
+#addtion of numbers
+num1 = 534
+num2 = 323
+#Now to add two numbers:
+sum = num1 + num2
+#output of the operation which have been take placed
+print("the sum of {0} and {1} is {2}".format(num1,num2,sum))
