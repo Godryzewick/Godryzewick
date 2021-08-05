6d8346c0cb7ef7a8a5be3751bd037976338738dd
Godryzewick@ <godwintitus05@gmail.com>
2021-08-05 15:48:03 +0530

    inspirtations@

---
+++ starting python/py inspirations/browser_access.py
@@ -0,0 +1,4 @@
+import os
+
+os.system('start chrome https://xeldrago-service.netlify.app/')
+
--- starting python/trials.py
+++ starting python/trials.py
@@ -3,6 +3,10 @@ print("welcome to math with kat")
 num1 = 534
 num2 = 323
 #Now to add two numbers:
-sum = num1 + num2
+sum = num1+num2
 #output of the operation which have been take placed
 print("the sum of {0} and {1} is {2}".format(num1,num2,sum))
+print(f"the sum of {num1} and {num2} is {sum}")
+q=input("a number\n")
+r=input("another number\n")
+print(int(q)+int(r))
